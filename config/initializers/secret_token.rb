8631c4d928ca09eaf3d88d320cfceace47b77db9
gem install -l rack-1.0.0.gem <>
2013-04-17 13:51:06 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '00613feb094504ec666fb3bea905f0449ba7ff58a8da015a62b75baca25e811aaf94825676870846ec35c2fcd3f9f9a8a593fd301d63b8fae08d5d4893a1b8f8'
